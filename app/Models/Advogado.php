914208aa542fba598d2f88c4ac0b8b8a1a157b36
Eliezer de Almeida <eliezerdealmeida62@gmail.com>
2024-06-18 19:20:18 -0300

    criado models advogado e grupousuario

---
+++ app/Models/Advogado.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Models;
+namespace App\Casts;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+
+class Advogado extends Authenticatable
+{
+    use Notifiable, SoftDeletes;
+
+    /**
+     * Primary key de Advogado
+     * 
+     * @var string
+     */
+    protected $primaryKey = 'id';
+
+    /**
+     * Ativa criado em e atualizado na tabela
+     * 
+     * @var bool
+     */
+    public $timestamps = true;
+    
+    /**
+     * Os atributos do model Advogado.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'xid',
+        'cpf',
+        'nome',
+        'email',
+        'password',
+        'oab',
+        'uf_oab',
+        'grupo',
+        'primeiro_login'
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+        'id'
+    ];
+
+    /**
+     * Serializacao dos Accessors
+     * 
+     * @var array<int, string>
+     */
+    protected $appends = [
+
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime:d/m/Y',
+            'password' => 'hashed',
+            'xid' => 'string',
+            'cpf' => 'string',
+            'nome' => 'string',
+            'email' => 'string',
+            'oab' => 'string',
+            'uf_oab' => 'string',
+            'primeiro_login' => 'bool',
+            'created_at' => 'datetime:d/m/Y HH:MM:SS',
+            'updated_at' => 'datetime:d/m/Y HH:MM:SS'
+        ];
+    }
+
+
+    public function grupo()
+    {
+        return $this->belongsTo(Advogado::class);
+    }
+}
---
+++ app/Models/GrupoUsuario.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class GrupoUsuario extends Model
+{
+    /**
+     * Primary key de grupo
+     * 
+     * @var string
+     */
+    protected $primaryKey = 'grupo';
+
+    /**
+     * Atributos de Grupo de Usuario
+     * 
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'grupo',
+        'descricao'
+    ];
+
+    /**
+     * Obtem todos os advogados do Grupo de usuario
+     * 
+     * @return Array<int, Advogado::class>
+     */
+    public function advogados()
+    {
+        return $this->hasMany(Advogado::class);
+    }
+}
