2556c9c2f4c80e3ba29adee8d2f9691a71846438
Eliezer de Almeida <eliezerdealmeida62@gmail.com>
2024-06-21 23:43:05 -0300

    Criado cidades

---
+++ app/Models/Cidade.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Cidade extends Model
+{
+    /**
+     * Tabela do model
+     * @var string
+     */
+    protected $table = 'cidades';
+
+    /**
+     * ID da cidade
+     * @var string
+     */
+    protected $primaryKey = 'id';
+
+    /**
+     * Dados do model
+     * @var string[]
+     */
+    protected $fillable = [
+        'nome',
+        'estado'
+    ];
+
+    /**
+     * Retorna o estado do model
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function estado()
+    {
+        return $this->belongsTo(Estado::class, 'estado');
+    }
+
+}
