83cadcb4cf58c9efb8129181df0ff54739f51f45
Eliezer de Almeida <eliezerdealmeida62@gmail.com>
2024-06-22 19:48:36 -0300

    Criado classe judicial

---
+++ app/Models/ClasseJudicial.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ClasseJudicial extends Model
+{
+    protected $table = 'classes_judiciais';
+
+    protected $primaryKey = 'id';
+
+    protected $fillable = [
+        'descricao'
+    ];
+}
