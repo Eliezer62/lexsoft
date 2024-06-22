a17131914fc355253216c18992c434770ee3f595
Eliezer de Almeida <eliezerdealmeida62@gmail.com>
2024-06-22 19:01:22 -0300

    criado telefones

---
+++ database/migrations/2024_06_22_214945_create_telefones.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('telefones', function (Blueprint $table) {
+            $table->id();
+            $table->char('xid')
+                ->unique('uc_telefones_xid');
+            $table->char('ddi', 4)->default('+55');
+            $table->char('ddd');
+            $table->char('numero', 12);
+            $table->integer('pessoafis')->nullable();
+            $table->integer('pessoajur')->nullable();
+
+            //constraint
+            $table->foreign('pessoafis')
+                ->references('id')
+                ->on('clientes_pessoa_fis');
+
+            $table->foreign('pessoajur')
+                ->references('id')
+                ->on('clientes_pessoa_jur');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('telefones');
+    }
+};
