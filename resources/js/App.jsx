40a7f6a5eeb50a8f9962170bc0eb8c46ac4ecec5
Eliezer de Almeida <eliezerdealmeida62@gmail.com>
2024-06-24 20:24:48 -0300

    add .css e bootstrap para app

--- resources/js/App.jsx
+++ resources/js/App.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LayoutBasico from "@/componentes/LayoutBasico.jsx";
+import '../css/app.css';
+import 'bootstrap';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <BrowserRouter>
             <Routes>
-                <Route path='/'/>
+                <Route path='/' Component={LayoutBasico}/>
             </Routes>
         </BrowserRouter>
     </React.StrictMode>
