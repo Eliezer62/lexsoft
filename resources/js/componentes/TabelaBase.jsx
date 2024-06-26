b4e02f04226f61769134a4a71f0f5a9963385e27
Eliezer de Almeida <eliezerdealmeida62@gmail.com>
2024-06-26 00:19:52 -0300

    add tabela base

---
+++ resources/js/componentes/TabelaBase.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import {Button, Col, Input, Layout, Row, Table} from 'antd';
+import { IoSearch } from "react-icons/io5";
+
+
+export default function TabelaBase(props)
+{
+
+    return (
+        <Layout
+            style={{
+                borderRadius: 10,
+                height:'90%'
+            }}
+        >
+            <Row
+                style={{padding:25}}
+            >
+                <Col span={8} offset={7}><Input placeholder={'Pesquisar'}/></Col>
+                <Col span={1}><Button><IoSearch/></Button></Col>
+                <Col span={1} offset={6}><Button type={'primary'} onClick={props.adicionar}>Adicionar</Button></Col>
+            </Row>
+            <Table
+                columns={props.coluna}
+                dataSource={props.dados}
+                size={'small'}
+                pagination={
+                {
+                    pageSize: 7
+                }
+                }
+                loading={props.loading}
+            />
+        </Layout>
+    );
+}
