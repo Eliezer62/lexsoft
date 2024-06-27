a8d5765b5b8384625eb529d0f40e375f3ab86159
Eliezer de Almeida <eliezerdealmeida62@gmail.com>
2024-06-27 01:37:27 -0300

    add fetch para enviar adv

--- resources/js/Advogados.jsx
+++ resources/js/Advogados.jsx
@@ -1,10 +1,11 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import LayoutBasico from "@/componentes/LayoutBasico.jsx";
 import TabelaBase from "@/componentes/TabelaBase.jsx";
 import {GrEdit, GrFormClose, GrView} from "react-icons/gr";
 import {IoIosRemoveCircleOutline} from "react-icons/io";
-import { Button, Modal } from "antd";
+import {Button, Form, message, Modal} from "antd";
 import NovoAdvogado from './componentes/advogados/NovoAdvogado';
+import axios from "axios";
 
 
 const colunas = [
@@ -44,75 +45,19 @@ const colunas = [
     }
 ]
 
-const teste = [
-    {
-        'nome':'Eliezer',
-        'cpf':'11312199962',
-        'grupo':'administrador'
-    },
-    {
-        'nome':'Eliezer',
-        'cpf':'11312199962',
-        'grupo':'administrador'
-    },
-    {
-        'nome':'Eliezer',
-        'cpf':'11312199962',
-        'grupo':'administrador'
-    },
-    {
-        'nome':'Eliezer',
-        'cpf':'11312199962',
-        'grupo':'administrador'
-    },
-    {
-        'nome':'Eliezer',
-        'cpf':'11312199962',
-        'grupo':'administrador'
-    },
-    {
-        'nome':'Eliezer',
-        'cpf':'11312199962',
-        'grupo':'administrador'
-    },
-    {
-        'nome':'Eliezer',
-        'cpf':'11312199962',
-        'grupo':'administrador'
-    },
-    {
-        'nome':'Eliezer',
-        'cpf':'11312199962',
-        'grupo':'administrador'
-    },
-    {
-        'nome':'Tom',
-        'cpf':'11312155914',
-        'grupo':'administrador'
-    },
-    {
-        'nome':'Eliezer',
-        'cpf':'05814468955',
-        'grupo':'administrador'
-    },
-    {
-        'nome':'Luiz',
-        'cpf':'11312199962',
-        'grupo':'administrador'
-    },
-    {
-        'nome':'Mario',
-        'cpf':'11312199962',
-        'grupo':'administrador'
-    },
-]
-
 const Advogados = () => {
+    const [time, setTime] = useState(Date.now());
     const [openNovoAdv, setOpenNovoAdv] = useState(false);
     const [confirmNovoAdv, setConfirmNovoAdv] = useState(false);
+    const [advogados, setAdvogados] = useState([]);
+    const [loadingTable, setLoadingTable] = useState(true);
+    const [advogado, setAdvogado] = useState({});
+    const [form] = Form.useForm();
+    const [messageApi, contextHolder] = message.useMessage();
 
     const adicionar = () =>
     {
+        setAdvogado({});
         setOpenNovoAdv(true);
     }
 
@@ -122,23 +67,60 @@ const Advogados = () => {
         }
     }
 
-    const enviarNovoAdv = () => {
+    const enviarNovoAdv =  () => {
         setConfirmNovoAdv(true);
+        form.validateFields().then(async ()=>{
+            console.log(advogado);
+            const response = await axios({
+                method:'POST',
+                url:'/api/advogados/',
+                data:advogado
+            }).catch(e=>{
+                const response = e.response;
+                messageApi.error('Erro em salvar o advogado: '+response.data.msg);
+            });
+            if(response.status==201) messageApi.success('Sucesso em salvar advogado');
+            setConfirmNovoAdv(false);
+            setOpenNovoAdv(false);
+        }).catch(e=>{
+            setConfirmNovoAdv(false);
+        });
     }
+
+    useEffect(() => {
+        const getAdvs = async () => {
+            const response = await axios.get('/api/advogados');
+            setAdvogados(await response.data);
+        }
+
+        const interval = setInterval(() => {
+            getAdvs();
+            setLoadingTable(false);
+
+        }, 3000);
+
+        return () => {
+            clearInterval(interval);
+        }
+    });
 
     return (
       <LayoutBasico titulo={'Advogados'} menu={'advogados'}>
           <TabelaBase
               coluna={colunas}
-              dados={teste}
+              dados={advogados}
               titulo={'Advogados'}
               adicionar={adicionar}
-          />
+              loading={loadingTable}
+          />{contextHolder}
           <NovoAdvogado
             open={openNovoAdv}
             handleCancel={cancelar}
             confirmLoading={confirmNovoAdv}
             handleOk={enviarNovoAdv}
+            advogado={advogado}
+            setAdvogado={setAdvogado}
+            form={form}
           />
       </LayoutBasico>
     );
