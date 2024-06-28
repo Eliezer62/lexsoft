0645a1d3aa983b4efa0a62a2c3dceb7ad9da3314
Eliezer de Almeida <eliezerdealmeida62@gmail.com>
2024-06-28 16:43:42 -0300

    tela de visualizacao de cliente

--- resources/js/Clientes.jsx
+++ resources/js/Clientes.jsx
@@ -1,11 +1,17 @@
 import React, {useState} from "react";
 import LayoutBasico from "@/componentes/LayoutBasico.jsx";
 import TabelaBase from "@/componentes/TabelaBase.jsx";
+import {Button, Col, Drawer, Row} from "antd";
+import {GrEdit, GrView} from "react-icons/gr";
+import {IoIosRemoveCircleOutline} from "react-icons/io";
+import DescricaoItem from "@/componentes/DescricaoItem.jsx";
 
 const Clientes = () => {
     const [pesquisa, setPesquisa] = useState('');
     const [clientes, setClientes] = useState([]);
     const [loadingTable, setLoadingTable] = useState(false);
+    const [visualizarClienteState, setVisualizarClienteState] = useState(false);
+    const [cliente, setCliente] = useState({});
 
     const colunas = [
         {
@@ -30,17 +36,75 @@ const Clientes = () => {
             sortDirections: ['descend', 'ascend'],
             filteredValue:[pesquisa],
             onFilter: (value, record) => record.email.startsWith(value) ||  record.nome.toLowerCase().includes(value.toLowerCase()) || record.documento === value,
+        },
+        {
+            title:'Ações',
+            key:'acoes',
+            fixed:'center',
+            render:(_, record) => {
+                return (
+                    <>
+                        <Button onClick={()=>{
+                            setCliente(record);
+                            setVisualizarClienteState(true);
+                        }}
+                        ><GrView/></Button>&nbsp;
+                        <Button><GrEdit/></Button>&nbsp;
+                        <Button danger={true}><IoIosRemoveCircleOutline/></Button>
+                    </>
+                )
+            }
         }
+    ];
+
+
+    const data = [
+        {
+            nome:'eliezer',
+            documento:'11312199962',
+            email:"eliezerdealmeida62@gmail.com"
+        },
+        {
+            nome:'Gnoatto Advocacia',
+            documento:'04361527000100',
+            email:"eliezerdealmeida62@gmail.com"
+        },
     ];
 
     return (
       <LayoutBasico titulo={'Clientes'} menu={'clientes'}>
         <TabelaBase
             coluna={colunas}
-            dados={clientes}
+            dados={data}
             loading={loadingTable}
-            pesquisa={pesquisa}
+            pesquisa={setPesquisa}
         />
+          <Drawer
+              closable={true}
+              destroyOnClose={true}
+              title={'Visualizar Cliente'}
+              placement="right"
+              open={visualizarClienteState}
+              loading={false}
+              size={'large'}
+              onClose={() => setVisualizarClienteState(false)}
+          >
+              <Row>
+                  <Col span={12}>
+                      <DescricaoItem titulo={'Nome'} conteudo={cliente.nome}/>
+                  </Col>
+              </Row>
+              <Row>
+                  <Col span={12}>
+                      <DescricaoItem titulo={'CPF/CNPJ'} conteudo={cliente.documento}/>
+                  </Col>
+              </Row>
+              <Row>
+                  <Col span={12}>
+                      <DescricaoItem titulo={'email'} conteudo={cliente.email}/>
+                  </Col>
+              </Row>
+          </Drawer>
       </LayoutBasico>
     );
 }
