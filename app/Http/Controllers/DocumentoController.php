a05961904ac5f69555dd79c3e357dd80ba1499f0
Eliezer de Almeida <eliezerdealmeida62@gmail.com>
2024-07-05 18:15:02 -0300

    salvando documento no banco de dados

--- app/Http/Controllers/DocumentoController.php
+++ app/Http/Controllers/DocumentoController.php
@@ -2,6 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\ClientePessoaFis;
+use App\Models\ClientePessoaJur;
+use App\Models\Documento;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 
@@ -9,13 +13,35 @@ class DocumentoController extends Controller
 {
     public function store(Request $request)
     {
-        $valido = $request->validate(['arquivo'=>'required|max:2048576']);
+        $valido = $request->validate([
+            'arquivo'=>'required|max:2048576',
+            'descricao'=>'required|string',
+            'cliente'=>'required',
+            'tipo'=>'required'
+        ]);
         if($request->hasFile('arquivo'))
         {
             $arquivo = $request->file('arquivo');
             $nome = $arquivo->hashName();
             $upload = Storage::disk('local')->put('/content/', $arquivo);
-            return Storage::disk('local')->get('/content/'.$nome);
+            $dados = [
+                'descricao'=>$request->input('descricao'),
+                'src'=>'/content/'.$nome,
+                'data_criado'=>Carbon::parse(Storage::lastModified('/content/'.$nome))->timezone('America/Sao_Paulo'),
+                'tipo_arquivo'=>$arquivo->extension(),
+                'pessoafis'=>null,
+                'pessoajur'=>null
+            ];
+            if($request->input('tipo')=='fisico')
+            {
+                $dados['pessoafis'] = ClientePessoaFis::firstWhere('xid', $request->input('cliente'))->id;
+            }
+            if($request->input('tipo')=='juridico')
+            {
+                $dados['pessoajur'] = ClientePessoaJur::firstWhere('xid', $request->input('cliente'))->id;
+            }
+            $doc = Documento::create($dados);
+            response(status: 200);
         }
         else return response(status: 500);
     }
