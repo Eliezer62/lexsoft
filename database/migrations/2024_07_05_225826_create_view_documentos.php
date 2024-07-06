4dbb2610f5742f841fae471bc89e6a60652be48a
Eliezer de Almeida <eliezerdealmeida62@gmail.com>
2024-07-05 23:06:00 -0300

    criado migration para criar a view documentos

---
+++ database/migrations/2024_07_05_225826_create_view_documentos.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        DB::statement("CREATE VIEW view_documentos AS SELECT doc.xid, doc.descricao, doc.data_criacao,
+            COALESCE((SELECT nome FROM clientes_pessoa_fis cf WHERE cf.id = doc.pessoafis),(SELECT razao_social FROM clientes_pessoa_jur cj WHERE cj.id=doc.pessoajur))
+            AS cliente,
+            COALESCE((SELECT cpf FROM clientes_pessoa_fis cf WHERE cf.id = doc.pessoafis),(SELECT cnpj FROM clientes_pessoa_jur cj WHERE cj.id=doc.pessoajur))
+            AS documento
+            FROM documentos doc;");
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        DB::statement('DROP VIEW IF EXISTS view_documentos;');
+    }
+};
