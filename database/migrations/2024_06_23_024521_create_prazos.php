03e6ce508c12ead13c3f9f99ba982a06e3928855
Eliezer de Almeida <eliezerdealmeida62@gmail.com>
2024-07-08 20:14:00 -0300

    fix softdelete prazos

--- database/migrations/2024_06_23_024521_create_prazos.php
+++ database/migrations/2024_06_23_024521_create_prazos.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('prazos', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->softDeletes();
             $table->string('descricao')->nullable();
             $table->datetime('inicio')->nullable();
             $table->datetime('fim')->nullable();
