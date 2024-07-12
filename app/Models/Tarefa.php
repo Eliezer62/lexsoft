8ce013d1d49a1a594bb3eda1acd77c8674f713e3
eliezer-debug <20017826@uepg.br>
2024-07-12 00:49:00 -0300

    add delete em cascade para prazos a partir do model

--- app/Models/Tarefa.php
+++ app/Models/Tarefa.php
@@ -5,9 +5,11 @@ namespace App\Models;
 use App\Enum\TarefaStatus;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Tarefa extends Model
 {
+    use SoftDeletes;
     /**
      * Primary key de tarefa
      * @var string
@@ -31,6 +33,10 @@ class Tarefa extends Model
         'status'=>TarefaStatus::class
     ];
 
+    public $cascadeDelete = [
+        'hasOne' => \App\Models\Prazo::class,
+    ];
+
     /**
      * Retorna o responsavel da tarefa
      * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
@@ -43,5 +49,14 @@ class Tarefa extends Model
     public function prazo()
     {
         return $this->hasOne(Prazo::class, 'tarefa', 'id');
+    }
+
+    public static function boot()
+    {
+        parent::boot();
+        // cause a delete of a product to cascade to children so they are also deleted
+        static::deleted(function ($tarefa) {
+            $tarefa->prazo->delete();
+        });
     }
 }
