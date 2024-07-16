23c2b86db0134dc0a4d3f26edbaea38de5b6fcd9
eliezer-debug <eliezerdealmeida62@gmail.com>
2024-07-16 06:58:47 -0300

    add get processos na tabela

--- resources/js/Processos.jsx
+++ resources/js/Processos.jsx
@@ -1,9 +1,15 @@
 import React, {useEffect, useState} from 'react';
 import LayoutBasico from "@/componentes/LayoutBasico.jsx";
 import TabelaBase from "@/componentes/TabelaBase.jsx";
+import axios from "axios";
+import {message} from "antd";
 
 export default function Processos() {
     const [pesquisa, setPesquisa] = useState('');
+    const [timeOut, setTimeOut] = useState(0);
+    const [loadingTable, setLoadingTable] = useState(true);
+    const [processos, setProcessos] = useState([]);
+    const [messageApi, contextHolder] = message.useMessage();
 
     const colunas = [
         {
@@ -13,7 +19,7 @@ export default function Processos() {
             sorter: (a, b) => a.numero.localeCompare(b.numero),
             sortDirections: ['descend', 'ascend'],
             filteredValue:[pesquisa],
-            onFilter: (value, record) => record.numero.includes(value) || record.numCNJ.includes(value)
+            onFilter: (value, record) => record.numero.includes(value) || record.numCNJ.includes(value) || record.tribunal.includes(value)
         },
         {
             title:'Numeração CNJ',
@@ -41,12 +47,35 @@ export default function Processos() {
         }
     ]
 
+
+    useEffect(() => {
+        const getProcessos = async () => {
+            await axios.get('/api/processos').then(resp => {
+                setProcessos(resp.data);
+            }).catch((e)=>{
+                messageApi.error('Erro em obter os processos');
+            });
+        }
+        const interval = setInterval(()=>{
+            getProcessos();
+            setTimeOut(3000);
+        }, timeOut);
+
+        return () => {
+            clearInterval(interval);
+            setLoadingTable(false);
+        }
+    });
+
     return (
       <LayoutBasico titulo="Processos" menu='processos'>
+          {contextHolder}
             <TabelaBase
                 pesquisa={setPesquisa}
                 coluna={colunas}
+                dados={processos}
                 adicionar={()=>window.location.href='/processos/criar'}
+                loading={loadingTable}
             />
       </LayoutBasico>
     );
