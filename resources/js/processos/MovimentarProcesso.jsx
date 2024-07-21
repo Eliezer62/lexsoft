8f73b0a4d6291c3a42ea901ea11b25458b5a6d9a
eliezer-debug <eliezerdealmeida62@gmail.com>
2024-07-21 18:41:57 -0300

    add enviar eventos de processo

--- resources/js/processos/MovimentarProcesso.jsx
+++ resources/js/processos/MovimentarProcesso.jsx
@@ -1,16 +1,22 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import LayoutBasico from "@/componentes/LayoutBasico.jsx";
 import {useParams} from 'react-router-dom';
-import {Breadcrumb, Button, DatePicker, Flex, Form, Input, Modal, Row, Table} from "antd";
+import {Breadcrumb, Button, DatePicker, Flex, Form, Input, Modal, message, Table, Popover, Popconfirm} from "antd";
 import {HomeOutlined} from "@ant-design/icons";
 import dayjs from "dayjs";
+import axios from "axios";
+import {IoIosRemoveCircleOutline} from "react-icons/io";
 
 export default function MovimentarProcesso()
 {
     const {xid} = useParams();
     const [pesquisa, setPesquisa] = useState('');
-    const [novoEvento, setNovoEvento] = useState(true);
+    const [novoEvento, setNovoEvento] = useState(false);
     const [form] = Form.useForm();
+    const [confirmLoading, setConfirmLoading] = useState(false);
+    const [messageApi, context] = message.useMessage();
+    const [eventos, setEventos] = useState([]);
+    const [intervalo, setIntervalo] = useState(0);
 
     const colunas = [
         {
@@ -23,20 +29,26 @@ export default function MovimentarProcesso()
         {
             title:'Descrição',
             key:'descricao',
+            dataIndex: 'descricao',
             sorter: (a, b) => a.descricao.localeCompare(b.descricao),
         },
         {
             title:'Data',
             key:'data',
+            dataIndex: 'data'
         },
         {
             title:'Prazo',
             children:[
                 {
-                    title:'Início'
+                    title:'Início',
+                    key:'inicio',
+                    dataIndex:'inicio'
                 },
                 {
-                    title: 'Fim'
+                    title: 'Fim',
+                    key:'fim',
+                    dataIndex: 'fim'
                 }
             ]
         },
@@ -46,6 +58,24 @@ export default function MovimentarProcesso()
             render: (_, record) => {
                 return (
                     <>
+                        <Button title={'vincular documento'}>Documento</Button>&nbsp;
+                        <Popconfirm title={'Remover evento'}
+                                    description={'Deseja remover este evento?'}
+                                    onConfirm={async ()=>{
+                                        const loa = messageApi.loading('Removendo evento');
+                                        await axios.delete('/api/processos/'+xid+'/eventos/'+record.xid)
+                                            .then(()=>{
+                                                messageApi.destroy(loa.id);
+                                                messageApi.success('Evento removindo com sucesso');
+                                            })
+                                            .catch(()=>{
+                                                messageApi.destroy(loa.id);
+                                                messageApi.error('Erro em remover evento');
+                                            });
+                                    }}
+                        >
+                            <Button danger title={'remover'}><IoIosRemoveCircleOutline/></Button>
+                        </Popconfirm>
                     </>
                 )
             }
@@ -56,6 +86,55 @@ export default function MovimentarProcesso()
         setNovoEvento(false);
     }
 
+    const enviar = () => {
+        form.validateFields().then(async ()=>{
+            setConfirmLoading(true);
+            let evento = {};
+            evento.descricao = form.getFieldValue('descricao');
+            evento.data = dayjs(form.getFieldValue('data'), 'DD/MM/YYYY').format('YYYY-MM-DD');
+            let prazo = form.getFieldValue('prazo');
+            if(prazo)
+            {
+                evento.inicio = dayjs(prazo[0], 'DD/MM/YYYY HH:mm').format('YYYY-MM-DD HH:mm');
+                evento.fim = dayjs(prazo[1], 'DD/MM/YYYY HH:mm').format('YYYY-MM-DD HH:mm');
+            }
+
+            await axios({
+                url:'/api/processos/'+xid+'/eventos',
+                method:'POST',
+                data:evento
+            }).then((resp)=>{
+                setConfirmLoading(false);
+                setNovoEvento(false);
+                messageApi.success('Evento salvo com sucesso');
+            }).catch((e)=>{
+                setConfirmLoading(false);
+                messageApi.error('Erro em salvar o evento: '+e.response.data?.msg);
+            });
+        });
+        setConfirmLoading(false);
+    }
+
+    useEffect(() => {
+        const getEventos = async () => {
+            await axios.get('/api/processos/'+xid+'/eventos')
+                .then((resp)=>{
+                    resp.data.forEach(e => {
+                        e.inicio = (e.inicio)?dayjs(e.inicio, 'YYYY-MM-DD HH:mm').format('DD/MM/YYYY HH:mm'):null;
+                        e.fim = (e.fim)?dayjs(e.fim, 'YYYY-MM-DD HH:mm').format('DD/MM/YYYY HH:mm'):null;
+                    });
+                    setEventos(resp.data);
+                });
+        }
+        const intervaloFc = setInterval(()=>{
+            getEventos();
+            setIntervalo(3000);
+        }, intervalo);
+
+        return ()=>{
+            clearInterval(intervaloFc);
+        }
+    });
 
     return (
         <LayoutBasico titulo={'Movimentar Processo'} menu={'processos'}>
@@ -65,6 +144,7 @@ export default function MovimentarProcesso()
                 {title:'Movimentar'},
             ]}
             />
+            {context}
             <Flex justify={'end'} align={'middle'}>
                 <Input.Search placeholder={'Pesquisar'}
                               style={{width:'250px', marginRight: '10px'}}
@@ -77,12 +157,18 @@ export default function MovimentarProcesso()
             <Table
                 columns={colunas}
                 size={'small'}
+                dataSource={eventos}
+                pagination={{
+                    pageSize: 7
+                }}
             />
             <Modal
                 open={novoEvento}
                 title={'Adicionar evento'}
                 onCancel={fechar}
                 destroyOnClose={true}
+                confirmLoading={confirmLoading}
+                onOk={enviar}
             >
                 <Form
                     form={form}
