f92ad8101e45beb97856001672acd0ea2705f1b9
Eliezer de Almeida <eliezerdealmeida62@gmail.com>
2024-07-24 19:15:23 -0300

    add tela para criar um novo documento de texto

---
+++ resources/js/componentes/documentos/NovoDocumento.jsx
@@ -0,0 +1,103 @@
+import React, {useEffect} from 'react';
+import LayoutBasico from "@/componentes/LayoutBasico.jsx";
+import {Button, Flex, Form, Input, message, Modal} from "antd";
+import Editor from "@/componentes/Editor.jsx";
+import {useParams} from 'react-router-dom';
+import axios from "axios";
+
+export default function NovoDocumento()
+{
+    const [conteudo, setConteudo] = React.useState(null);
+    const [documento, setDocumento] = React.useState('');
+    const [modal, contextModal] = Modal.useModal();
+    const [form] = Form.useForm();
+    const [messageApi, contextMsg] = message.useMessage();
+    const {xid} = useParams();
+    const {evento} = useParams();
+
+    const salvar = async () => {
+        if(!documento?.xid)
+        modal.confirm({
+            title:'Salvar documento',
+            content: (
+                <Form form={form} layout={'vertical'}>
+                    <Form.Item
+                        label={'Nome do arquivo'}
+                        name={'nome'}
+                        rules={[
+                            {required:true, message:'Nome do arquivo é obrigatório'},
+                            {max:60, message:'Tamanho máximo de nome é 60 caracteres'}
+                        ]}
+                    >
+                        <Input/>
+                    </Form.Item>
+                </Form>
+            )
+        }).then(async ()=>{
+            const file = new File([conteudo], form.getFieldValue('nome')+'.lex' , {
+                type: "text/plain",
+            });
+            let formData = new FormData();
+            formData.append('arquivo', file);
+            formData.append('nome', form.getFieldValue('nome'));
+
+            await axios({
+                method:'POST',
+                url:`/api/processos/${xid}/movimentar/${evento}/salvar-documento`,
+                data:formData
+            }).then((resp)=>{
+                setDocumento(resp.data);
+                messageApi.success('Documento salvo com sucesso');
+            }).catch(()=>{
+                messageApi.error('Erro em salvar o documento');
+            });
+        });
+        else {
+            const file = new File([conteudo], form.getFieldValue('nome')+'.lex' , {
+                type: "text/plain",
+            });
+            let formData = new FormData();
+            formData.append('arquivo', file);
+            formData.append('nome', form.getFieldValue('nome'));
+            formData.append('xid', documento.xid);
+
+            await axios({
+                method:'POST',
+                url:`/api/processos/${xid}/movimentar/${evento}/salvar-documento`,
+                data:formData
+            }).then((resp)=>{
+                setDocumento(resp.data);
+                messageApi.success('Documento salvo com sucesso');
+            }).catch(()=>{
+                messageApi.error('Erro em salvar o documento');
+            });
+        }
+    }
+
+    useEffect(() => {
+        const intervalo = setInterval(()=>{
+            if(documento?.xid)
+            {
+                messageApi.info('Salvamento automático', 1);
+                salvar();
+            }
+        }, 30000);
+
+        return () => {
+            clearInterval(intervalo);
+        }
+    });
+
+    return (
+        <LayoutBasico menu={'processos'}>
+            <Flex justify={'right'}>
+                <Button style={{marginRight:'10px'}}>Exportar PDF</Button>
+                <Button type={'primary'} onClick={salvar}>Salvar</Button>
+            </Flex>
+            <br/>
+            <Editor change={setConteudo}/>
+            {contextModal}
+            {contextMsg}
+        </LayoutBasico>
+    );
+}
