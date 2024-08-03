f273bbc507848294e7f1acbb26992b1c98cfdfe9
Eliezer de Almeida <eliezerdealmeida62@gmail.com>
2024-08-03 18:22:23 -0300

    add dashboard

---
+++ resources/js/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import LayoutBasico from "@/componentes/LayoutBasico.jsx";
+
+export default function Dashboard()
+{
+
+    return (
+        <LayoutBasico titulo={'Dashboard'} menu={'dashboard'}>
+
+        </LayoutBasico>
+    )
+}
