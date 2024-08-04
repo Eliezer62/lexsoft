5bbb2f7e97555dc18cd64ddc58165a028799a331
Eliezer de Almeida <eliezerdealmeida62@gmail.com>
2024-08-03 22:00:28 -0300

    add middleware para validar transacoes conforme grupo de usuario

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\ValidarPermissoes;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -14,6 +15,12 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->validateCsrfTokens(except: [
             '/storage/*'
+        ]);
+        $middleware->alias([
+            'permissoes'=>ValidarPermissoes::class
+        ]);
+        $middleware->api(prepend:[
+            ValidarPermissoes::class
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
