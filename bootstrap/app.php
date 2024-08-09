d795da3fd6b7fc829596f6b1e0a0d1c43571109d
Eliezer de Almeida <eliezerdealmeida62@gmail.com>
2024-08-08 22:04:54 -0300

    add task schedule para enviar prazos

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,7 @@ use App\Http\Middleware\ValidarPermissoes;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Console\Scheduling\Schedule;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -25,4 +26,8 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
-    })->create();
+    })->withSchedule(function (Schedule $schedule)
+    {
+        $schedule->command('emails:avisar-prazos')->daily();
+    })
+    ->create();
