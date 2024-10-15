97dce08084ba75ab53741f0c7477c66f984676f5
Eliezer de Almeida <eliezerdealmeida62@gmail.com>
2024-10-14 21:33:03 -0300

    add tela para negocios

--- resources/js/Negocios.tsx
+++ resources/js/Negocios.tsx
@@ -1,10 +1,79 @@
 import LayoutBasico  from './componentes/LayoutBasico'
+import {DndContext, useDraggable, useDroppable} from '@dnd-kit/core';
+import {CSS} from '@dnd-kit/utilities';
+import {useState} from "react";
+import Board from './componentes/negocios/Board';
+import {Button, Flex, message, Modal, Segmented} from "antd";
+import {AppstoreOutlined, BarsOutlined} from "@ant-design/icons";
+import NovoNegocio from "./componentes/negocios/NovoNegocio";
+function Draggable(props) {
+    const {attributes, listeners, setNodeRef, transform} = useDraggable({
+        id: props.id,
+    });
+    const style = {
+        // Outputs `translate3d(x, y, 0)`
+        transform: CSS.Translate.toString(transform),
+    };
+
+    return (
+        <button ref={setNodeRef} style={style} {...listeners} {...attributes}>
+            {props.children}
+        </button>
+    );
+}
+
+
+function Droppable(props) {
+    const {isOver, setNodeRef} = useDroppable({
+        id: props.id,
+    });
+    const style = {
+        opacity: isOver ? 1 : 0.5,
+    };
+
+    return (
+        <div ref={setNodeRef} style={style}>
+            {props.children}
+        </div>
+    );
+}
+
 
 export default function Negocios()
 {
+    const [parent, setParent] = useState(null);
+    const [exibicao, setExibicao] = useState('Kanban');
+    const [openNovo, setOpenNovo] = useState(true);
+    const [messageApi, context] = message.useMessage();
+
+    const draggable = (
+        <Draggable id="draggable">
+            Go ahead, drag me.
+        </Draggable>
+    );
+
+    function handleDragEnd({over}) {
+        setParent(over ? over.id : null);
+    }
     return (
         <LayoutBasico titulo = "Negócios" menu={"negocios"}>
-
+            {context}
+            <NovoNegocio open={openNovo} onCancel={()=>setOpenNovo(false)} message={messageApi}/>
+            <Flex justify={'right'} style={{marginBottom:'15px'}}><Button type={'primary'} onClick={()=>setOpenNovo(true)}>Novo Negócio</Button></Flex>
+            <Flex justify={'right'} style={{marginBottom:'15px'}}>
+                <Segmented
+                    options={[
+                        { value: 'Kanban', icon: <AppstoreOutlined />},
+                        { value: 'Lista', icon: <BarsOutlined />},
+                    ]}
+                    onChange={(v)=>setExibicao(v)}
+                />
+            </Flex>
+            {(exibicao=='Kanban')?
+                (<div className={"board"}>
+                    <Board/>
+                </div>):
+                (<></>)}
         </LayoutBasico>
     );
 }
---
+++ resources/js/componentes/negocios/NovoNegocio.jsx
@@ -0,0 +1,117 @@
+import React, {useEffect, useState} from 'react';
+import {DatePicker, Form, Modal, Select} from "antd";
+import EditorSimples from "../EditorSimples";
+import dayjs from "dayjs";
+import axios from "axios";
+
+export default function NovoNegocio(props)
+{
+    const [responsaveis, setResponsaveis] = useState([]);
+    const [clientes, setClientes] = useState([]);
+    const [confirm, setConfirm] = useState(false);
+    const [form] = Form.useForm();
+
+    const enviar = () => {
+        form.validateFields().then(async () => {
+            setConfirm(true);
+            const negocio = {};
+            negocio.descricao = form.getFieldValue('descricao');
+            negocio.data = dayjs(form.getFieldValue('data')).format('YYYY-MM-DD HH:mm');
+            negocio.responsavel = form.getFieldValue('responsavel')??null;
+            negocio.contatos = form.getFieldValue('contatos')??null;
+
+            await axios.post('/api/negocios', negocio)
+                .then((response) => {
+                    props.message.success('Negócio salvo com sucesso');
+                    props.onCancel();
+                    setConfirm(false);
+                }).catch((e) => {
+                    props.message.error('Erro em salvar o negócio: '+e.response?.data?.msg);
+                    setConfirm(false);
+                })
+        });
+    }
+
+    useEffect(() => {
+        const resp = async () => {
+            await axios.get('/api/advogados')
+                .then((response) => {
+                    let temp = [];
+                    response.data.forEach(adv => {
+                       temp.push({label:adv.nome, value:adv.xid});
+                    });
+                    setResponsaveis(temp);
+                })
+                .catch((erro) => {
+                    props.message.error('Erro em obter os dados');
+                });
+        }
+        const clientes = async () => {
+            await axios.get('/api/clientes')
+                .then((response) => {
+                    const tmp = [];
+                    response.data.forEach((cliente) => {
+                        tmp.push({value:cliente.xid, label:cliente.nome+ ' '+cliente.documento, tipo:cliente.tipo});
+                    })
+                    setClientes(tmp);
+                }).catch((e) => props.message.error('Erro em obter os dados'));
+        }
+
+        resp();
+        clientes();
+    }, []);
+
+    return (
+      <Modal
+        title={'Novo Negócio'}
+        open={props.open}
+        onCancel={props.onCancel}
+        onOk={enviar}
+        confirmLoading={confirm}
+      >
+        <Form
+            form={form}
+            preserve={false}
+            layout={'vertical'}
+        >
+            <Form.Item
+                label={'Descrição'}
+                name={"descricao"}
+                rules={[
+                    {required:true, message:'Descrição é obrigatório'}
+                ]}
+            >
+                <EditorSimples />
+            </Form.Item>
+
+            <Form.Item
+                label={'Data'}
+                name={'data'}
+                initialValue={dayjs()}
+                rules={[
+                    {required:true, message:'Data é obrigatório'}
+                ]}
+            >
+                <DatePicker
+                    showTime
+                    format={'DD/MM/YYYY HH:mm'}
+                    maxDate={dayjs()}
+                />
+            </Form.Item>
+
+            <Form.Item
+                label={'Responsável'}
+                name={'responsavel'}
+            >
+                <Select options={responsaveis}/>
+            </Form.Item>
+            <Form.Item
+                label={'Contatos'}
+                name={'contatos'}
+            >
+                <Select options={clientes} showSearch optionFilterProp={'label'} mode={'multiple'}/>
+            </Form.Item>
+        </Form>
+      </Modal>
+    );
+}
