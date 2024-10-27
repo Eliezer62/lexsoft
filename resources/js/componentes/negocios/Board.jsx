76c3850f62b5fa2fb00310847c0b01a088f9092c
Eliezer de Almeida <eliezerdealmeida62@gmail.com>
2024-10-27 18:15:06 -0300

    ref Board

--- resources/js/componentes/negocios/Board.jsx
+++ resources/js/componentes/negocios/Board.jsx
@@ -28,6 +28,7 @@ export default function Board(props)
 
     const dndEnd = (e) => {
         const { over, active } = e;
+        console.log(e);
         const msg = messageApi.loading('Atualizando a fase');
         axios.get(`/api/negocios/${active.id}/atualizar-fase/${over.id}`)
             .then((response) => {
@@ -39,83 +40,76 @@ export default function Board(props)
         });
     }
 
+    const colisao = (event) => {
+        const {droppableRects, pointerCoordinates, droppableContainers} = event;
+        droppableRects.forEach((value, key, map)=>{
+            if(value.left <= pointerCoordinates.y && value.right >= pointerCoordinates.y)
+            {
+                let coluna = droppableContainers.find(({id}) => id === key);
+                const height = coluna?.node?.current?.clientHeight;
+                coluna?.node?.current?.style.setProperty('height', height+'px');
+            }
+        });
+
+    }
+
     return (
         <Skeleton loading={props.loading}>
             {context}
-            <ul className={"board"}
+            <div className={"board"}
                 style={{
                     overflow: 'auto',
                     overflowX: "scroll",
-                    display: "block",
+                    display: "flex",
                     width: '100%',
-                    whiteSpace: 'nowrap'
+                    whiteSpace: 'nowrap',
+                    alignItems: 'flex-start'
                 }}>
                 <DndContext onDragEnd={dndEnd}>
                     <Coluna titulo={'Contato Inicial'} total={props.totalInicial} id={'inicial'}
                             content={props.inicial.map(item => (<Item descricao={item.descricao}
                                                                       id={item.xid}
-                                                                      data={dayjs(item.data).format('DD/MM/YYYY HH:mm')}
-                                                                      responsavel={item.responsavel}
-                                                                      prioridade={item.prioridade}
-                                                                      valor={item.valor}
+                                                                      item={item}
                             />))}
                     />
                     <Coluna titulo={'Qualificação'} id={'qualificacao'} total={props.totalQualificacao}
                             content={props.qualificacao.map(item => (<Item descricao={item.descricao}
-                                                                      id={item.xid}
-                                                                      data={dayjs(item.data).format('DD/MM/YYYY HH:mm')}
-                                                                      responsavel={item.responsavel}
-                                                                      prioridade={item.prioridade}
-                                                                      valor={item.valor}
+                                                                            id={item.xid}
+                                                                            item={item}
                             />))}
                     />
                     <Coluna titulo={'Avaliação'} id={'avaliacao'} total={props.totalAvaliacao}
                             content={props.avaliacao.map(item => (<Item descricao={item.descricao}
                                                                            id={item.xid}
-                                                                           data={dayjs(item.data).format('DD/MM/YYYY HH:mm')}
-                                                                           responsavel={item.responsavel}
-                                                                           prioridade={item.prioridade}
-                                                                           valor={item.valor}
+                                                                           item={item}
                             />))}
                     />
                     <Coluna titulo={'Proposta'} id={'proposta'} total={props.totalProposta}
                             content={props.proposta.map(item => (<Item descricao={item.descricao}
                                                                            id={item.xid}
-                                                                           data={dayjs(item.data).format('DD/MM/YYYY HH:mm')}
-                                                                           responsavel={item.responsavel}
-                                                                           prioridade={item.prioridade}
-                                                                           valor={item.valor}
+                                                                           item={item}
                             />))}
                     />
                     <Coluna titulo={'Negociação'} id={'negociacao'} total={props.totalNegociacao}
                             content={props.negociacao.map(item => (<Item descricao={item.descricao}
                                                                            id={item.xid}
-                                                                           data={dayjs(item.data).format('DD/MM/YYYY HH:mm')}
-                                                                           responsavel={item.responsavel}
-                                                                           prioridade={item.prioridade}
-                                                                           valor={item.valor}
+                                                                           item={item}
                             />))}
                     />
                     <Coluna titulo={'Fechado'} id={'fechado'} total={props.totalFechado}
                             content={props.fechado.map(item => (<Item descricao={item.descricao}
                                                                            id={item.xid}
-                                                                           data={dayjs(item.data).format('DD/MM/YYYY HH:mm')}
-                                                                           responsavel={item.responsavel}
-                                                                           prioridade={item.prioridade}
-                                                                           valor={item.valor}
+                                                                           item={item}
                             />))}
                     />
                     <Coluna titulo={'Perdido'} id={'perdido'} total={props.totalPerdido}
                             content={props.perdido.map(item => (<Item descricao={item.descricao}
                                                                            id={item.xid}
-                                                                           data={dayjs(item.data).format('DD/MM/YYYY HH:mm')}
-                                                                           responsavel={item.responsavel}
-                                                                           prioridade={item.prioridade}
-                                                                           valor={item.valor}
+                                                                           item={item}
                             />))}
                     />
                 </DndContext>
-            </ul>
+            </div>
         </Skeleton>
     );
 }
