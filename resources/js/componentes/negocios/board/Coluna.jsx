998b45c61f748576c8d2dd314bfe0ba12b5abd46
Eliezer de Almeida <eliezerdealmeida62@gmail.com>
2024-10-27 18:14:29 -0300

    refatoracao de codigo em board

--- resources/js/componentes/negocios/board/Coluna.jsx
+++ resources/js/componentes/negocios/board/Coluna.jsx
@@ -5,16 +5,16 @@ import {SortableContext} from '@dnd-kit/sortable'
 const Coluna = (props) =>
 {
     const estilo = {
-        width: '300px',
+        minWidth: '300px',
         marginRight: '30px',
         display: 'inline-block',
-        backgroundColor: '#f0f0f0'
+        backgroundColor: '#f0f0f0',
     }
 
     const contentCSS = {
         padding: '15px',
         backgroundColor: '#f0f0f0',
-        minHeight:'60%'
+        minHeight:'300px'
     }
 
     const {isOver, setNodeRef} = useDroppable({
@@ -23,7 +23,7 @@ const Coluna = (props) =>
 
 
     return (
-        <li style={estilo}>
+        <div style={estilo}>
             <div style={{padding: '15px', backgroundColor: '#c0c0c0'}}>
                 <h3>{props.titulo}</h3>
             </div>
@@ -33,7 +33,7 @@ const Coluna = (props) =>
             <div style={{border: '1px solid #c0c0c0', padding: '15px', minHeight: '10%'}}>
                 Total {new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(props.total??0)}
             </div>
-        </li>
+        </div>
     )
 }
 
--- resources/js/componentes/negocios/board/Item.tsx
+++ resources/js/componentes/negocios/board/Item.tsx
@@ -2,6 +2,7 @@ import {useDraggable} from "@dnd-kit/core";
 import {CSS} from "@dnd-kit/utilities";
 import {Button, Card} from "antd";
 import DOMPurify from 'dompurify';
+import dayjs from "dayjs";
 export default function Item(props)
 {
     const {attributes, listeners, setNodeRef, transform} = useDraggable({
@@ -11,19 +12,24 @@ export default function Item(props)
     const style = {
         // Outputs `translate3d(x, y, 0)`
         transform: CSS.Translate.toString(transform),
+        zIndex:'1'
     };
 
+    // @ts-ignore
     return (
         <Card ref={setNodeRef} style={style} {...listeners} {...attributes}>
             <Card.Meta
                 title={props.titulo}
                 description={
                 <>
-                    <p style={{color: '#0091ae', fontWeight: '500', boxSizing:'border-box', wordWrap:'anywhere', overflowWrap: 'break-word'}} dangerouslySetInnerHTML={{__html:DOMPurify.sanitize(props.descricao)}}></p>
-                    <p><span style={{fontWeight:'500', color: '#595959'}}>Data e Hora: </span>{props.data}</p>
-                    <p><span style={{fontWeight:'500', color: '#595959'}}>Responsável: </span>{props.responsavel}</p>
-                    <p><span style={{fontWeight:'500', color: '#595959'}}>Prioridade: </span>{props.prioridade}</p>
-                    <p><span style={{fontWeight: '500', color: '#595959'}}>Valor: </span>{new Intl.NumberFormat('pt-BR', { style: "currency", currency: "BRL" }).format(props.valor)}</p>
+                    <div style={{inlineSize: '50px', whiteSpace:'pre-wrap', color: '#0091ae', fontWeight: '500', wordBreak:'break-all', overflowWrap: 'break-word'}} dangerouslySetInnerHTML={{__html:DOMPurify.sanitize(props.item.descricao)}}></div>
+                    <p><span style={{fontWeight:'500', color: '#595959'}}>Data e Hora: </span>{dayjs(props.item.data).format('DD/MM/YYYY HH:mm')}</p>
+                    <p style={{whiteSpace:'pre-wrap'}}><span style={{fontWeight:'500', color: '#595959'}}>Responsável: </span>{props.item.responsavel}</p>
+                    <p><span style={{fontWeight:'500', color: '#595959'}}>Prioridade: </span>{props.item.prioridade}</p>
+                    <p><span style={{fontWeight: '500', color: '#595959'}}>Valor: </span>{new Intl.NumberFormat('pt-BR', { style: "currency", currency: "BRL" }).format(props.item.valor)}</p>
+                    <p><span style={{fontWeight: '500', color: '#595959'}}>Contatos: </span>{(props.item?.contatos?.map(contato =>
+                        (<p style={{whiteSpace:'pre-wrap'}}>{contato?.nome}</p>)
+                    ))}</p>
                 </>
                 }
             />
